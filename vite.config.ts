b67b6d130df2210b8ea873195a3717aac9a6f316
Salomoncrack <salomoncrack0102@gmail.com>
2026-02-24 23:37:00 -0400

    configurando base URL para Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/app/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
